219875f710c2860849845b374ac1a62d6174c376
Thomas Rushton <trushton134@gmail.com>
2016-08-11 10:43:16 -0700

    I'm going through changes man

---
+++ Assets/randomSpawn.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class randomSpawn : MonoBehaviour {
+	public List<Vector3> spawnPoints;
+
+
+	// Use this for initialization
+	void Start () {
+		int r = Random.Range (0, spawnPoints.Count);
+		Vector3 spawnPoint = spawnPoints [r];
+		gameObject.transform.position = spawnPoint;
+	}
+
+}
